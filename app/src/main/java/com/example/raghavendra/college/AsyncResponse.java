2a842e2c62715c52b1e09cfecd87b48a0309df58
Raghavendra Gururaj <Raghavendra Gururaj>
2016-04-24 22:54:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/raghavendra/college/AsyncResponse.java
@@ -0,0 +1,9 @@
+package com.example.raghavendra.college;
+
+/**
+ * Created by akhil on 3/15/2016.
+ */
+public interface AsyncResponse {
+   public void processFinish(String result);
+
+}
---
+++ app/src/main/java/com/example/raghavendra/college/timetable_frag.java
@@ -0,0 +1,27 @@
+package com.example.raghavendra.college;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Raghavendra on 3/13/2016.
+ */
+public class timetable_frag extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        //System.out.println("YUP");
+        return inflater.inflate(R.layout.activity_timetable6b,container,false);
+        // return super.onCreateView(inflater,container,savedInstanceState);
+
+    }
+
+
+
+}
